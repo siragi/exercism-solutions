be4b534560aae140de3d90793de16712dd34f082
Simon Giger <simon.giger@tuta.io>
2021-01-12 16:37:13 +0100

    [+] dot-dsl: internal domain specific language - used to create graph data structures using the builder pattern:   In short, this pattern allows you to split the construction function of your struct, that contains a lot of arguments, into several separate functions. - demonstrates the use of derive proc macro, that can implement automatically a defined 'Assets' trait.

---
+++ rust/dot-dsl/src/lib.derivemacro.rs
@@ -0,0 +1,101 @@
+// Adaptation of nilsso's solution: https://exercism.io/tracks/rust/exercises/dot-dsl/solutions/caa9a67e77294429bfdd430eb8d7d052
+// and runiq's solution: https://exercism.io/tracks/rust/exercises/dot-dsl/solutions/7538eb532e23413ca9dea828d60ddc6d
+// Following the builder pattern: https://doc.rust-lang.org/1.0.0/style/ownership/builders.html
+
+// Attrs trait will be implemented via a proc_macro called Attr
+pub trait Attrs {
+    fn with_attrs(self, attrs: &[(&str, &str)]) -> Self;
+    fn get_attr(&self, key: &str) -> Option<&str>;
+}
+
+pub mod graph {
+    use graph_items::edge::Edge;
+    use graph_items::node::Node;
+    use std::collections::HashMap;
+
+    use super::Attrs;
+    use dot_dsl_derive::Attrs;
+
+    #[derive(Attrs)]
+    pub struct Graph {
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: HashMap<String, String>,
+    }
+
+    impl Graph {
+        pub fn new() -> Self {
+            Self {
+                nodes: Vec::new(),
+                edges: Vec::new(),
+                attrs: HashMap::new(),
+            }
+        }
+
+        pub fn with_nodes(self, nodes: &[Node]) -> Self {
+            Self {
+                nodes: nodes.into(),
+                ..self
+            }
+        }
+
+        pub fn with_edges(self, edges: &[Edge]) -> Self {
+            Self {
+                edges: edges.into(),
+                ..self
+            }
+        }
+
+        pub fn get_node(&self, name: &str) -> Option<&Node> {
+            self.nodes.iter().find(|node| node.name == name)
+        }
+    }
+
+    pub mod graph_items {
+        pub mod node {
+            use std::collections::HashMap;
+
+            use super::super::super::Attrs;
+            use dot_dsl_derive::Attrs;
+
+            #[derive(Attrs, Clone, PartialEq, Debug)]
+            pub struct Node {
+                pub name: String,
+                pub attrs: HashMap<String, String>,
+            }
+
+            impl Node {
+                pub fn new(name: &str) -> Self {
+                    Self {
+                        name: String::from(name),
+                        attrs: HashMap::new(),
+                    }
+                }
+            }
+        }
+
+        pub mod edge {
+            use std::collections::HashMap;
+
+            use super::super::super::Attrs;
+            use dot_dsl_derive::Attrs;
+
+            #[derive(Attrs, Clone, PartialEq, Debug)]
+            pub struct Edge {
+                pub from: String,
+                pub to: String,
+                pub attrs: HashMap<String, String>,
+            }
+
+            impl Edge {
+                pub fn new(from: &str, to: &str) -> Self {
+                    Self {
+                        from: from.to_string(),
+                        to: to.to_string(),
+                        attrs: HashMap::new(),
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ rust/dot-dsl/src/lib.struct.rs
@@ -0,0 +1,101 @@
+pub mod graph {
+    use graph_items::edge::Edge;
+    use graph_items::node::Node;
+    use std::collections::HashMap;
+
+    pub struct Graph {
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: HashMap<String, String>,
+    }
+
+    impl Graph {
+        pub fn new() -> Self {
+            // Construct a new Graph struct."
+            Self {
+                nodes: Vec::new(),
+                edges: Vec::new(),
+                attrs: HashMap::new(),
+            }
+        }
+
+        pub fn with_nodes(mut self, nodes: &Vec<Node>) -> Self {
+            self.nodes = nodes.clone();
+            self
+        }
+
+        pub fn with_edges(mut self, edges: &Vec<Edge>) -> Self {
+            self.edges = edges.clone();
+            self
+        }
+
+        pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+            for (key, value) in attrs {
+                self.attrs.insert(key.to_string(), value.to_string());
+            }
+            self
+        }
+
+        pub fn get_node(&self, name: &str) -> Option<&Node> {
+            self.nodes.iter().find(|node| node.name == name)
+        }
+    }
+
+    pub mod graph_items {
+        pub mod node {
+            use std::collections::HashMap;
+            #[derive(Clone, PartialEq, Debug)]
+            pub struct Node {
+                pub name: String,
+                pub attrs: HashMap<String, String>,
+            }
+
+            impl Node {
+                pub fn new(name: &str) -> Self {
+                    Self {
+                        name: String::from(name),
+                        attrs: HashMap::new(),
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+                    for (key, value) in attrs {
+                        self.attrs.insert(key.to_string(), value.to_string());
+                    }
+                    self
+                }
+
+                pub fn get_attr(&self, attr: &str) -> Option<&str> {
+                    self.attrs.get(attr).map(|value| value.as_str())
+                }
+            }
+        }
+
+        pub mod edge {
+            use std::collections::HashMap;
+            #[derive(Clone, PartialEq, Debug)]
+            pub struct Edge {
+                pub leftnode: String,
+                pub rightnode: String,
+                pub attrs: HashMap<String, String>,
+            }
+
+            impl Edge {
+                pub fn new(leftnode: &str, rightnode: &str) -> Self {
+                    Self {
+                        leftnode: leftnode.to_string(),
+                        rightnode: rightnode.to_string(),
+                        attrs: HashMap::new(),
+                    }
+                }
+
+                pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+                    for (key, value) in attrs {
+                        self.attrs.insert(key.to_string(), value.to_string());
+                    }
+                    self
+                }
+            }
+        }
+    }
+}
