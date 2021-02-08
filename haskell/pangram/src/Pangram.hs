1786afb8b27b993efe987abb7a9e893eb70db6d0
Simon Giger <simon.giger@tuta.io>
2021-02-08 23:41:46 +0100

    [+] feature: [Pangram] checks for Sentences with all letters in the alphabet - Learning Haskell: Bullet Programming, η-Reduction, Functors (<$>), Reversing Prefix to Infix Functions and vice versa

---
+++ haskell/pangram/src/Pangram.hs
@@ -0,0 +1,34 @@
+module Pangram (isPangram) where
+
+import Data.Char
+import Data.List (intersect, nub) -- only needed for last solution
+
+isPangram :: String -> Bool
+-- isPangram = (`all` ['a' .. 'z']) . (flip elem . map toLower)
+-- isPangram text = ((`all` ['a' .. 'z']) . (flip elem . map toLower)) text
+-- isPangram text = (`all` ['a' .. 'z']) ((flip elem . map toLower) text) -- f(g x)
+-- isPangram text = (flip elem . map toLower) text `all` ['a' .. 'z']
+-- isPangram text = all ((flip elem . map toLower) text) ['a' .. 'z']
+-- isPangram text = all (`elem` map toLower text) ['a' .. 'z']
+-- isPangram text = all (\char -> char `elem` map toLower text) ['a' .. 'z']
+
+{- isPangram = and . (<$> ['a' .. 'z']) . isElem
+  where
+    isElem text = (`elem` map toLower text) -}
+
+{- isElem :: [Char] -> Char -> Bool
+isElem text = (`elem` map toLower text) -}
+-- isPangram = and . (<$> ['a' .. 'z']) . isElem
+-- isPangram text = ((and . (<$> ['a' .. 'z'])) . (isElem)) text
+-- isPangram text = (and . (<$> ['a' .. 'z'])) (isElem text)
+-- isPangram text = and ((isElem text) <$> ['a' .. 'z'])
+-- isPangram text = and ((<$>) (isElem text) ['a' .. 'z'])
+-- isPangram text = and ((<$>) (`elem` map toLower text) ['a' .. 'z'])
+-- isPangram text = and ((`elem` map toLower text) <$> ['a' .. 'z'])
+
+-- "there should be 26 . elements in . the unique list of . elements filtered on (being between a & z) . from the input where each element is transformed to lower case
+isPangram = (== 26) . length . nub . filter (`elem` ['a' .. 'z']) . map toLower
+
+-- isPangram text = 26 == length (nub $ ['a' .. 'z'] `intersect` map toLower text)
+-- isPangram text = 26 == length (nub (['a' .. 'z'] `intersect` map toLower text))
+-- isPangram text = length (nub (['a' .. 'z'] `intersect` map toLower text)) == 26
